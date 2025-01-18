6bf455b11a10d5377602d0d11814edf6711412a7
JuanEPB <JuanEPB20@outlook.com>
2025-01-18 13:07:21 -0600

    Initial commit

---
+++ src/main/java/edu/utvt/clase1/HelloWorld.java
@@ -0,0 +1,8 @@
+package edu.utvt.clase1;
+
+public class HelloWorld {
+    public static void main (String [] args){
+        System.out.println("Hola Mundo!!!");
+    }
+
+}
---
+++ src/main/java/edu/utvt/clase1/variables.java
@@ -0,0 +1,19 @@
+package edu.utvt.clase1;
+
+public class variables {
+    public static void main(String[] args) {
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+
+        int initValue;
+        initValue = 100;
+        System.out.println(initValue);
+
+        long hugeValue = 100000000L;
+        System.out.println(hugeValue);
+
+        float gloatVar = 3.14f;
+        System.out.println(gloatVar);
+
+    }
+}
